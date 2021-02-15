99d5087f695c367966882cc7b0c6860a4137753e
rolandcsa <roland.csatlos8@protonmail.com>
2021-02-15 16:15:44 +0100

    c2

--- src/Kristof.java
+++ src/Kristof.java
@@ -3,7 +3,7 @@ public class Kristof {
     public void namePrinter(){
         System.out.println("Kristof Papp");
     }
-    public void cheer() {
-        System.out.println("Jade");
+    public void cheer(String className) {
+        System.out.println(className);
     }
 }
