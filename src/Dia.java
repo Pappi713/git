6526a4191ff57a993ca0786166d2505e4e144065
Diana Szanto <szanto.diana.1992@gmail.com>
2021-02-15 16:53:36 +0100

    b5

--- src/Dia.java
+++ src/Dia.java
@@ -1,16 +1,15 @@
 public class Dia {
 
     public static void main(String[] args) {
-        System.out.println(count(1992));
+        System.out.println(count(1992, 2021));
         Kristof kristof = new Kristof();
         System.out.println(kristof.cheer("java"));
     }
 
-    public static Integer count(int birthyear) {
-        int presentYear = 2021;
+    public static Integer count(int birthYear, int presentYear) {
 
         System.out.println("08");
-        return presentYear - birthyear;
+        return presentYear - birthYear;
 
     }
 }
