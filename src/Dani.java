2a09d7b2a9b56c8c5a96638e30fc7b7363cc73f1
Diana Szanto <szanto.diana.1992@gmail.com>
2021-02-15 16:34:37 +0100

    e1

--- src/Dani.java
+++ src/Dani.java
@@ -1,17 +1,6 @@
 public class Dani {
-
-<<<<<<< HEAD
-    public void future() {
-        System.out.println("Pig");
-    }
-
     public void future(String horoscope) {
         System.out.println(horoscope);
-=======
-    public void future(){
-        System.out.println("Pig");
-
->>>>>>> ec7adbdba69889fff68064c2bb5ae9a448f1ae10
     }
 }
 
--- src/Kristof.java
+++ src/Kristof.java
@@ -1,14 +1,12 @@
 public class Kristof {
-
-<<<<<<< HEAD
     private String name;
 
-    public void greet(){
+    public void greet() {
         System.out.println("Greetings!");
-=======
+    }
+
     public void namePrinter(String name) {
         System.out.println(name);
->>>>>>> ec7adbdba69889fff68064c2bb5ae9a448f1ae10
     }
 
     public String cheer(String language) {
