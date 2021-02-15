78ec0b6ae19ac1ac634dbf4f6f0bc3a6e9a003dd
Diana Szanto <szanto.diana.1992@gmail.com>
2021-02-15 16:31:51 +0100

    b4

--- src/Dani.java
+++ src/Dani.java
@@ -1,12 +1,11 @@
 public class Dani {
 
-<<<<<<< HEAD
-    public void future(){
+    public void future() {
         System.out.println("Pig");
-=======
-    public void future(String horoscope){
-        System.out.println(horoscope);
->>>>>>> 6841746b7b815b5b40a0e9c66559f59c06ba98b1
     }
 
+    public void future(String horoscope) {
+        System.out.println(horoscope);
+    }
 }
+
