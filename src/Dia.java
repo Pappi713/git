d9887c6659a15b6a7ba270e845df378077d64886
Diana Szanto <szanto.diana.1992@gmail.com>
2021-02-15 16:17:30 +0100

    b2

--- src/Dia.java
+++ src/Dia.java
@@ -1,11 +1,10 @@
 public class Dia {
     public static void main(String[] args) {
-        System.out.println(count());
+        System.out.println(count(1992));
     }
 
-    public static Integer count() {
-        int birthYear = 1992;
+    public static Integer count(int birthyear) {
         int presentYear = 2021;
-        return presentYear - birthYear;
+        return presentYear - birthyear;
     }
 }
