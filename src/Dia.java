f905bcd77f9f4d16d8708682c993ba2aaca49fbf
Pappi713 <papp.kristof713@gmail.com>
2021-02-15 16:49:31 +0100

    asd

--- src/Dia.java
+++ src/Dia.java
@@ -8,14 +8,7 @@ public class Dia {
 
     public static Integer count(int birthyear) {
         int presentYear = 2021;
-<<<<<<< HEAD
-        System.out.println("08");
-        return presentYear - 1996;
-=======
-
         System.out.println("08");
         return presentYear - birthyear;
-
->>>>>>> 2a09d7b2a9b56c8c5a96638e30fc7b7363cc73f1
     }
 }
